a44cb5ef4b85f7383b004a588bd3de0c567ebcb5
adh1ka <hafizhadhi27@gmail.com>
2024-09-21 17:52:35 +0700

    Add files via upload

--- Minggu4/453.c
+++ Minggu4/453.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    float a, b, hasil;
-    char operator;
-    printf("Input angka pertama : ");
-    scanf("%d", &a);
-    printf("Masukkan operator (+, -, *, /) : ");
-    scanf("%c", &operator);
-    switch(operator) {
-        case '+': hasil = a + b; break;
-        case '-': hasil = a - b; break;
-        case '*': hasil = a * b; break;
-        case '/': hasil = a / b; break;
-        default: printf("Invalid operator");
+    float angka1, angka2, hasil;
+    char operator, pilihan;
+
+    printf("Input 's' untuk masuk ke program\n Input 'e' untuk keluar dari program\n Input s/e : ");
+    scanf("%c", &pilihan);
+
+    if (pilihan == 's'){
+        goto kalkulator;
     }
+    
+    if (pilihan == 'e'){
+        goto selesai;
+    }
+
+    kalkulator:
+        printf("Masukkan operasi yang diinginkan (misal 10 + 4) : ");
+        scanf("%f %c %f", &angka1, &operator, &angka2);
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;
+        } else {
+            printf("Masukkan operator dengan benar!!!!!\n");
+        }
+
+        printf("Hasilnya adalah %.2lf\n", hasil);
+
+    selesai:
+        printf("Program selesai\n");
     return 0;
 }
