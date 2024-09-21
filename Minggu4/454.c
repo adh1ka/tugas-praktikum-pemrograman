628bbf8f90557a65c41a2c2640dbf4f341a1d30c
adh1ka <hafizhadhi27@gmail.com>
2024-09-21 19:16:25 +0700

    Update 454.c

--- Minggu4/454.c
+++ Minggu4/454.c
@@ -5,12 +5,16 @@ int main() {
     printf("Input tahun : ");
     scanf("%d", &tahun);
     if (tahun < 1600 || tahun > 2400) {
-        printf("Masukkan tahun dengan benar\n");
+        printf("Masukkan tahun antara 1600 sampai 2400\n");
     } else {
-        if (tahun % 4 == 0){
-            printf("Tahun %d merupakan tahun kabisat\n", tahun);
+        if (tahun % 400 == 0){
+            printf("Tahun kabisat\n");
+        } else if (tahun % 100 == 0) {
+            printf("Bukan tahun kabisat\n");
+        } else if (tahun % 4 == 0) {
+            printf("Tahun kabisat\n");
         } else {
-            printf("Tahun %d bukan tahun kabisat\n", tahun);
+            printf("Bukan tahun kabisat\n");
         }
     }
     return 0;
