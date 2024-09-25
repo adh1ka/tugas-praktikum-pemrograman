def16a72384ac3526073ba77916746b683bcc70d
adh1ka <hafizhadhi27@gmail.com>
2024-09-25 09:41:10 +0700

    Create 554.c

---
+++ Minggu5/554.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, n = 10;
+    float celsius, fahrenheit, reamur, kelvin;
+
+    printf("No \t Celcius \t  Fahrenheit \t Reamur \t Kelvin\n");
+
+    for (i = 0; i <= n; i++) {
+        celsius = i * 10;
+        fahrenheit = (celsius * 9 / 5) + 32;
+        reamur = celsius * 4 / 5;
+        kelvin = celsius + 273.15;
+        printf("%-4d \t %-5.1f \t \t %-6.1f \t %-6.1f \t %-6.1f\n", i, celsius, fahrenheit, reamur, kelvin);
+    }
+    return 0;
+}
