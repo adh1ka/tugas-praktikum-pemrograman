b8e7c3d8c01a675c6909ae2944a3cd6c6685c22b
adh1ka <hafizhadhi27@gmail.com>
2024-09-25 08:19:26 +0700

    Update 551.c

--- Minggu5/551.c
+++ Minggu5/551.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
-int main() {
 
-  return 0;
+int main() {
+    int kolom, baris, angka = 5;
+
+    for (baris = 0; baris < angka; baris++) {
+        for (kolom = 0; kolom < angka; kolom++){
+            if (baris == kolom) {
+                printf("1");
+            } else {
+                printf("0");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
 }
