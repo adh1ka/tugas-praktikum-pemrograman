f730aef316819eb5be0d68fa7a2dd1fd77c95c05
adh1ka <hafizhadhi27@gmail.com>
2024-09-28 04:56:10 +0700

    Update 557.c

--- Minggu5/557.c
+++ Minggu5/557.c
@@ -4,6 +4,7 @@ int main() {
     int i, n = 10;
     unsigned long long faktorial = 1;
     printf("%-10s %10s\n", "n", "n!");
+    
     for (i = 1; i <= n; i++) {
         faktorial *= i;
         printf("%-10d %10llu\n", i, faktorial);
