3534e71114844e4482b141bb0da2ba37b76474a4
adh1ka <hafizhadhi27@gmail.com>
2024-10-01 21:42:34 +0700

    Update 557.c

--- Minggu5/557.c
+++ Minggu5/557.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    int i, n = 10;
-    unsigned long long faktorial = 1;
-    printf("%-10s %10s\n", "n", "n!");
-    
-    for (i = 1; i <= n; i++) {
-        faktorial *= i;
-        printf("%-10d %10llu\n", i, faktorial);
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
     }
+    return result;
+}
+
+void print_factorial_table(int n) {
+    printf("%-4s %-30s %20s\n", "n!", "Ekspresi Perkalian", "Hasil Faktorial");
+    printf("-------------------------------------------------------------\n");
+    
+    for (int i = 1; i <= n; i++) {
+        printf("%-4d ", i);
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+            if (j != i) {
+                printf("x");
+            }
+        }
+        printf("%*s", 30 - (2 * i - 1), ""); 
+        unsigned long long fact = factorial(i);
+        printf("%20llu\n", fact);
+    }
+}
+
+int main() {
+    int n = 10;
+    print_factorial_table(n);
     return 0;
 }
