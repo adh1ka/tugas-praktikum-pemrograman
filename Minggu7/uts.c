45195e0aa38792d925d1dc551ba3f23105d209b3
adh1ka <hafizhadhi27@gmail.com>
2024-10-03 23:48:31 +0700

    Create uts.c

---
+++ Minggu7/uts.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char input;
+    int r;
+    double total = 0, data;
+    printf("------PROGRAM PENGHITUNG RESISTANSI------\n");
+    printf("Input jumlah resistor : ");
+    scanf("%d", &r);
+    printf("Rangkaian apa yang akan anda hitung resistansinya, Serial atau Paralel\n");
+    printf("Input 's' untuk serial\n Input 'p' untuk paralel\n");
+    printf("Input (s/p) : ");
+    scanf(" %c", &input);
+    if (input == 's' || input == 'S') {
+        for (int i = 1; i <= r; i++) {
+            printf("Input ohm resistor ke-%d : ", i);
+            scanf("%lf", &data);
+            total += data;
+        }
+        printf("Total resistansi dari rangkaian serial : %lf ohm\n", total);
+    } else if (input == 'p' || input == 'P') {
+        for (int i = 1; i <= r; i++) {
+            printf("Input ohm resistor ke-%d : ", i);
+            scanf("%lf", &data);
+            total += 1.0 / total;
+        }
+        total = 1.0 / data;
+        printf("Total resistansi dari rangkaian paralel : %lf ohm \n", total);
+    } else {
+        printf("Masukkan pilihan dengan benar!!!\n");
+    }
+    return 0;
+}
