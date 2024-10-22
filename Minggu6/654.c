17ad8ef9fb3f704ecc415dd4d4de8a97f834223c
adh1ka <hafizhadhi27@gmail.com>
2024-10-22 09:45:26 +0700

    Update 654.c

--- Minggu6/654.c
+++ Minggu6/654.c
@@ -1,43 +1,40 @@
-//BELUM SELESAI
-
 #include <stdio.h>
 
-float suhu(float num, char src, char dest) {
-    if (src == 'C' || src == 'c') {
-        if (dest == 'f' || dest == 'F') {
-            return 
-        } else if (dest == 'r' || dest == 'R') {
+float suhu(float t, char s, char d) {
+    float result;
 
-        } else {
-
-        }
-    } else if (src == 'r' || src == 'R') {
-        if (dest == 'c' || dest == 'C') {
-
-        } else if (dest == 'f' || dest == 'F') {
-
-        } else {
-
-        }
-    } else if (src == 'f' || src == 'F') {
-        if (dest == 'r' || dest == 'R') {
-
-        } else if (dest == 'c' || dest == 'C') {
-
-        } else {
-
-        }
+    if (s == 'C' && d == 'F') {
+        result = (t * 9.0 / 5.0) + 32;
+    } else if (s == 'C' && d == 'R') {
+        result = t * 4.0 / 5.0;
+    } else if (s == 'F' && d == 'C') {
+        result = (t - 32) * 5.0 / 9.0;
+    } else if (s == 'F' && d == 'R') {
+        result = (t - 32) * 4.0 / 9.0;
+    } else if (s == 'R' && d == 'C') {
+        result = t * 5.0 / 4.0;
+    } else if (s == 'R' && d == 'F') {
+        result = (t * 9.0 / 4.0) + 32;
     } else {
-
+        result = t;
     }
+    return result;
 }
 
 int main() {
-    float nilai_source;
-    char suhu_src, suhu_dest;
-    printf("Masukkan suhu sumber : ");
-    scanf("%d %c", &nilai_source, &suhu_src);
-    printf("Masukkan suhu tujuan : ");
-    scanf("%c", &suhu_dest);
+    float t, hasil;
+    char s, d;
+
+    printf("Masukkan temperatur: ");
+    scanf("%f", &t);
+
+    printf("Masukkan suhu asal (C/F/R): ");
+    scanf(" %c", &s);
+
+    printf("Masukkan suhu tujuan (C/F/R): ");
+    scanf(" %c", &d);
+
+    hasil = suhu(t, s, d);
+    printf("Hasil konversi: %.2f %c\n", hasil, d);
     return 0;
 }
